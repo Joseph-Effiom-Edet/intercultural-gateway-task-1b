813ceb62312213b6ffd0d8c97e7d7dafb56862c3
Joseph Effiom Edet <90644500+AwesomeEJE@users.noreply.github.com>
2022-12-08 15:32:23 +0100

    Create remote repo

---
+++ index.js
@@ -0,0 +1,12 @@
+var blanket = document.getElementById("item1");
+var bracelet = document.getElementById("item2");
+var cap = document.getElementById("item3");
+var mug = document.getElementById("item4");
+var flipFlop = document.getElementById("item5");
+var tShirt = document.getElementById("item6");
+
+var cart = [];
+
+blanket.addEventListener("click", addBlanketToCart);
+
+function addBlanketToCart() {}
